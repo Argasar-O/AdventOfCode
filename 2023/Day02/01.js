1774d4ebb72e5536f4a05a6b597997d726e6a968
Argasar <qntnmr@gmail.com>
2023-12-02 12:09:55 +0100

    Day 02

---
+++ 2023/Day02/01.js
@@ -0,0 +1,22 @@
+import {input} from './input';
+
+let result = 0;
+
+input.split('\n').forEach(game => {
+    const [gameId, set] = game.split(':');
+    const sets = set.split(';');
+    const id = gameId.match(/[0-9]+/g)[0];
+    const max = {red: 0, green: 0, blue: 0};
+    sets.forEach(set => {
+        const colors = set.split(',');
+        colors.forEach(colorLine => {
+            const color = colorLine.match(/(blue|green|red)/g)[0];
+            const number = parseInt(colorLine.match(/[0-9]+/g)[0]);
+            if (max[color] < number) max[color] = number;
+        });
+    });
+
+    if (max["red"] <= 12 && max["green"] <= 13 && max["blue"] <= 14)
+        result += parseInt(id);
+})
+console.log(result);
---
+++ 2023/Day02/02.js
@@ -0,0 +1,19 @@
+import {input} from './input';
+
+let result = 0;
+
+input.split('\n').forEach(game => {
+    const [gameId, set] = game.split(':')
+    const sets = set.split(';');
+    const max = {red: 0, green: 0, blue: 0}
+    sets.forEach(set => {
+        const colors = set.split(',');
+        colors.forEach(colorLine => {
+            const color = colorLine.match(/(blue|green|red)/g)[0];
+            const number = parseInt(colorLine.match(/[0-9]+/g)[0]);
+            if (max[color] < number) max[color] = number;
+        });
+    });
+    result += max['red'] * max['green'] * max["blue"];
+})
+console.log(result);
