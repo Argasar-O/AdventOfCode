4045a89e093702a991d8c665a0908b2f7a2097c3
Argasar <qntnmr@gmail.com>
2023-12-03 13:06:32 +0100

    Module

--- 2023/Day01/02.js
+++ 2023/Day01/02.js
@@ -1,4 +1,4 @@
-import {input} from "./input";
+import {input} from "./input.js"
 
 const numbers = {one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9' }
 let result = 0;
