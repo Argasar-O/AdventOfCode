45257c6cc0ec0116cb0004a300ccaf1afe3323b3
Argasar <qntnmr@gmail.com>
2023-12-05 11:07:45 +0100

    Day 05

---
+++ 2023/Day05/D05-01.js
@@ -0,0 +1,25 @@
+import {input, inputE} from "./D05-input.js";
+
+const categories = input.split("\n\n");
+const tableSeeds = []
+categories.shift().split(': ')[1].split(' ').forEach(seedNumber => {
+    tableSeeds.push([parseInt(seedNumber), parseInt(seedNumber), false]);
+});
+
+categories.forEach(category => {
+    const numbersLine = category.split("\n");
+    tableSeeds.forEach(ts => ts[2] = false);
+    numbersLine.forEach(numbersStr => {
+        const numbers = numbersStr.split(" ").map(str => parseInt(str))
+        let [source, maxSource, destinationBase] = [numbers[1], numbers[1] + numbers[2], numbers[0]];
+
+        tableSeeds.forEach(seedMap => {
+            if (!seedMap[2] && seedMap[1] >= source && seedMap[1] <= maxSource) {
+                seedMap[1] = seedMap[1] + (destinationBase - source);
+                seedMap[2] = true;
+            }
+        });
+    });
+})
+
+console.log(Math.min(...tableSeeds.map(ts => ts[1])));
